be644b39bb1617895605ebe5c780014b6480c64a
shupp <shupp@d814430e-4d21-0410-b85c-fa13d367221e>
2006-12-18 02:37:58 +0000

    set logged_in_as after successful auth
    git-svn-id: file:///Users/bill/svn/mboxsvn/toasteradmin@38 d814430e-4d21-0410-b85c-fa13d367221e

--- trunk/Framework/Auth/vpopmail.php
+++ trunk/Framework/Auth/vpopmail.php
@@ -9,6 +9,7 @@ abstract class Framework_Auth_vpopmail extends Framework_Auth
         $encryptedPass = $this->session->__get('password');
         $plainPass =  $this->user->decryptPass($encryptedPass, (string)Framework::$site->config->mcryptKey);
         if($this->user->authenticate($this->session->__get('email'), $plainPass)) {
+            $this->setData('logged_in_as', $this->session->__get('email'));
             return true;
         } else {
             header('Location: ./?module=Login');
