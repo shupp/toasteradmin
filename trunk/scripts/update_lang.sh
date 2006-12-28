65a743ec15ea1a0f68000501632e2771e2921b67
shupp <shupp@d814430e-4d21-0410-b85c-fa13d367221e>
2006-12-28 20:42:57 +0000

    updated update_lang.sh script
    git-svn-id: file:///Users/bill/svn/mboxsvn/toasteradmin@62 d814430e-4d21-0410-b85c-fa13d367221e

--- trunk/scripts/update_lang.sh
+++ trunk/scripts/update_lang.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-MFILE='locale/en/LC_MESSAGES/messages.po'
+LCDIR='../Framework/Site/toasteradmin/locale/en/LC_MESSAGES'
+MFILE="$LCDIR/messages.po"
 
-xgettext -L PHP --keyword=_ public/index.php Includes/*php Modules/*php tpl/* --output=$MFILE
-sed -e 's/CHARSET/UTF-8/' $MFILE > $MFILE.new
-mv $MFILE.new $MFILE
-(cd locale/en/LC_MESSAGES/ ; msgfmt messages.po)
+TEMPFILE="/tmp/gettext_files.$$"
+
+find ../ | egrep -v '(.svn|Templates/Default/templates_c|Templates/Default/cache)' | egrep '(.php$)' > $TEMPFILE
+
+#xgettext -L PHP --keyword=_ public/index.php Includes/*php Modules/*php tpl/* --output=$MFILE
+xgettext -L PHP --keyword=_ -f $TEMPFILE --output=$MFILE
+sed -i -e 's/CHARSET/UTF-8/' $MFILE
+sed -i -e 's!FULL NAME <EMAIL@ADDRESS>!Bill Shupp <hostmaster@shupp.org>!' $MFILE
+(cd $LCDIR ; msgfmt messages.po)
+rm $TEMPFILE
