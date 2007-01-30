ef8b88951c8bc876731e379533ff5c6c0480632c
shupp <shupp@d814430e-4d21-0410-b85c-fa13d367221e>
2007-01-30 05:19:56 +0000

    add HTML_QuickForm_Renderer_AssocArray
    git-svn-id: file:///Users/bill/svn/mboxsvn/toasteradmin@77 d814430e-4d21-0410-b85c-fa13d367221e

---
+++ trunk/HTML/QuickForm/Renderer/AssocArray.php
@@ -0,0 +1,34 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * HTML_QuickForm_Renderer_AssocArray 
+ * 
+ * @package 
+ * @version $id$
+ * @copyright 2005-2006 Bill Shupp
+ * @author Bill Shupp <hostmaster@shupp.org> 
+ * @license GPL 2.0  {@link http://www.gnu.org/licenses/gpl.txt}
+ */
+/**
+ * HTML_QuickForm_Renderer_AssocArray 
+ * 
+ * @package 
+ * @version $id$
+ * @copyright 2005-2006 Bill Shupp
+ * @author Bill Shupp <hostmaster@shupp.org> 
+ * @license GPL 2.0  {@link http://www.gnu.org/licenses/gpl.txt}
+ */
+class HTML_QuickForm_Renderer_AssocArray {
+
+    static public function toAssocArray($array) {
+        $assocElements = array();
+        foreach ($array['elements'] as $key => $ar) {
+            $assocElements[$ar['name']] = $ar;
+        }
+        $array['assocElements'] = $assocElements;
+        return $array;
+    }
+
+}
