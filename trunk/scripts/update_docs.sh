0025287753abf9bea50cf74b583cfe5796f114cd
shupp <shupp@d814430e-4d21-0410-b85c-fa13d367221e>
2007-02-05 06:01:28 +0000

    update docs script
    git-svn-id: file:///Users/bill/svn/mboxsvn/toasteradmin@85 d814430e-4d21-0410-b85c-fa13d367221e

--- trunk/scripts/update_docs.sh
+++ trunk/scripts/update_docs.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
-cd /Users/shupp/web/mailtool || exit 99
+#cd /Users/shupp/web/mailtool || exit 99
+
+FILES="./htdocs/index.php"
+for i in `find Framework/ -name *.php | grep -v templates_c ` ; do 
+    FILES="$FILES,$i"
+done
+for i in `find HTML/ -name *.php | grep -v templates_c ` ; do 
+    FILES="$FILES,$i"
+done
+
 rm -rf doc
-phpdoc -s off -ti 'ToasterAdmin Documentation' -dn 'ToasterAdmin' -t ./doc -f ./README -d ./Includes/,./Modules,./public -ric README -o HTML:frames:DOM/phphtmllib,HTML:Smarty:default,HTML:frames:DOM/earthli,HTML:Smarty:PHP,HTML:Smarty:HandS,HTML:frames:phpedit,HTML:frames:DOM/l0l33t,HTML:frames:DOM/default,HTML:frames:earthli
+phpdoc \
+    -s off \
+    -ti 'ToasterAdmin Documentation' \
+    -dn 'ToasterAdmin' \
+    -t ./doc \
+    -f ./README,$FILES \
+    -ric README \
+    -o HTML:frames:DOM/earthli
+    #-o HTML:frames:DOM/phphtmllib,HTML:Smarty:default,HTML:frames:DOM/earthli,HTML:Smarty:PHP,HTML:Smarty:HandS,HTML:frames:phpedit,HTML:frames:DOM/l0l33t,HTML:frames:DOM/default,HTML:frames:earthli \
 
 #HTML:frames:phpedit
 #HTML:frames:l0l33t  apple
