ea64324e0222ea34fb8d8cb8592ddffebfd27dc4
shupp <shupp@d814430e-4d21-0410-b85c-fa13d367221e>
2007-02-08 02:04:44 +0000

    enable file source in phpdoc, update script
    git-svn-id: file:///Users/bill/svn/mboxsvn/toasteradmin@98 d814430e-4d21-0410-b85c-fa13d367221e

--- trunk/scripts/update_docs.sh
+++ trunk/scripts/update_docs.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #cd /Users/shupp/web/mailtool || exit 99
 
+DOCSDIR='./doc'
+
 FILES="./htdocs/index.php"
 for i in `find Framework/ -name *.php | grep -v templates_c ` ; do 
     FILES="$FILES,$i"
@@ -9,15 +11,18 @@ for i in `find HTML/ -name *.php | grep -v templates_c ` ; do
     FILES="$FILES,$i"
 done
 
-rm -rf doc
 phpdoc \
-    -s off \
+    -s on \
     -ti 'ToasterAdmin Documentation' \
     -dn 'ToasterAdmin' \
-    -t ./doc \
+    -t $DOCSDIR.tmp \
     -f ./README,$FILES \
     -ric README \
     -o HTML:frames:DOM/earthli
+
+mv $DOCSDIR $DOCSDIR.old
+mv $DOCSDIR.tmp $DOCSDIR
+rm -rf $DOCSDIR.old
     #-o HTML:frames:DOM/phphtmllib,HTML:Smarty:default,HTML:frames:DOM/earthli,HTML:Smarty:PHP,HTML:Smarty:HandS,HTML:frames:phpedit,HTML:frames:DOM/l0l33t,HTML:frames:DOM/default,HTML:frames:earthli \
 
 #HTML:frames:phpedit
