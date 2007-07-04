393417ade4f6c68b5aad47c28685df7a75255b6d
shupp <shupp@d814430e-4d21-0410-b85c-fa13d367221e>
2007-07-04 06:31:43 +0000

    update index.php to turn of E_NOTICE reporting, and add note about local PEAR install setting for include_path
    git-svn-id: file:///Users/bill/svn/mboxsvn/toasteradmin@182 d814430e-4d21-0410-b85c-fa13d367221e

--- trunk/htdocs/index.php
+++ trunk/htdocs/index.php
@@ -10,13 +10,21 @@
  * @filesource
  */
 
+ini_set('error_reporting', E_ALL & ~E_NOTICE);
+
 if(!isset($_GET['module'])) {
     header("Location: ./?module=Login");
     exit;
 }
 
 define('FRAMEWORK_BASE_PATH',dirname(__FILE__) . '/..');
-ini_set('include_path', FRAMEWORK_BASE_PATH . PATH_SEPARATOR . ini_get('include_path'));
+$ta_include_path = FRAMEWORK_BASE_PATH;
+// If you are running a local PEAR install, uncomment the next line
+// and edit it accordingly
+
+// $ta_include_path .= PATH_SEPARATOR . '/Users/shupp/pear/lib';
+
+ini_set('include_path', $ta_include_path . PATH_SEPARATOR . ini_get('include_path'));
 
 require_once('Framework.php');
 
