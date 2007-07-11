1f73b0ef96a37128a63f625f0cf6699499ffaae6
shupp <shupp@d814430e-4d21-0410-b85c-fa13d367221e>
2007-07-11 23:45:00 +0000

    do MailingLists module skeleton
    git-svn-id: file:///Users/bill/svn/mboxsvn/toasteradmin@190 d814430e-4d21-0410-b85c-fa13d367221e

---
+++ trunk/Framework/Module/MailingLists.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Framework_Module_MailingLists 
+ * 
+ * @uses Framework_Auth_User
+ * @package ToasterAdmin
+ * @copyright 2007 Bill Shupp
+ * @author Bill Shupp <hostmaster@shupp.org> 
+ * @license GPL 2.0  {@link http://www.gnu.org/licenses/gpl.txt}
+ */
+
+/**
+ * Framework_Module_MailingLists 
+ * 
+ * Management of ezmlm-idx lists through vpopmaild
+ * 
+ * @uses Framework_Auth_User
+ * @package ToasterAdmin
+ * @copyright 2007 Bill Shupp
+ * @author Bill Shupp <hostmaster@shupp.org> 
+ * @license GPL 2.0  {@link http://www.gnu.org/licenses/gpl.txt}
+ */
+class Framework_Module_MailingLists extends Framwork_Auth_User
+{
+    public function __construct() {
+        parent::__construct();
+        // Check that the domain was supplied
+        if (($result = $this->noDomainSupplied() {
+            return $result;
+        }
+        // Check that they have domain edit privilges
+        if (($result = $this->noDomainPrivs() {
+            return $result;
+        }
+        $this->user->setDefaultEzmlmOpts();
+    }
+
+    public function __default() {
+        return $this->listLists();
+    }
+
+    protected function checkPrivileges() {
+    }
+
+    public function listLists() {
+    }
+
+    public function addList() {
+    }
+
+    public function addListNow() {
+    }
+
+    public function modifyList() {
+    }
+
+    public function modifyListNow() {
+    }
+
+    public function deleteList() {
+    }
+
+    public function deleteListNow() {
+    }
+
+    public function deleteListCancel() {
+    }
+}
+?>
+
