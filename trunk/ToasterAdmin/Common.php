dee5029f9ea23e3df870f211bfc65d0b89dc310c
shupp <shupp@d814430e-4d21-0410-b85c-fa13d367221e>
2007-07-11 23:39:13 +0000

    Added ToasterAdmin_Common class, will centralize common module tasks here
    git-svn-id: file:///Users/bill/svn/mboxsvn/toasteradmin@188 d814430e-4d21-0410-b85c-fa13d367221e

---
+++ trunk/ToasterAdmin/Common.php
@@ -0,0 +1,56 @@
+<?php
+
+class ToasterAdmin_Common extends Framework_User_ToasterAdmin
+{
+
+   public function paginate($total) {
+        $this->setData('total', $total);
+        $this->setData('limit', (integer)Framework::$site->config->maxPerPage);
+        if (isset($_REQUEST['start']) && !ereg('[^0-9]', $_REQUEST['start'])) {
+            if ($_REQUEST['start'] == 0) {
+                $start = 1;
+            } else {
+                $start = $_REQUEST['start'];
+            }
+        }
+        if (!isset($start)) $start = 1;
+        $this->setData('start', $start);
+        $this->setData('currentPage', ceil($this->data['start'] / $this->data['limit']));
+        $this->setData('totalPages', ceil($this->data['total'] / $this->data['limit']));
+    }
+
+    /**
+     * noDomainPrivs 
+     * 
+     * Simple wrapper for isDomainAdmin that can be called in constructors
+     * 
+     * @access protected
+     * @return mixed PEAR_Error if they do NOT have domain admin privs, false if they do
+     */
+    protected function noDomainPrivs() {
+        // Verify that they have access
+        if (!$this->user->isDomainAdmin($this->domain)) {
+            return PEAR::raiseError(_('Error: you do not have edit privileges on domain ') . $this->domain);
+        }
+        return false;
+    }
+
+    /**
+     * noDomainSupplied 
+     * 
+     * Was $_REQUEST['domain'] supplied?
+     * Required by several modules
+     *
+     * @access protected false if it was supplied, PEAR_Error if it was not
+     * @return mixed
+     */
+    protected function noDomainSupplied() {
+        if (!isset($_REQUEST['domain'])) {
+            return PEAR::raiseError(_('Error no domain supplied'));
+        }
+        return false;
+    }
+
+}
+?>
+
