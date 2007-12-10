2645db1f1e7fb398fcf9a056adfc4b892a20aaff
shupp <shupp@d814430e-4d21-0410-b85c-fa13d367221e>
2007-12-10 16:22:11 +0000

    get find domain working - fix typo in form action url
    git-svn-id: file:///Users/bill/svn/mboxsvn/toasteradmin@230 d814430e-4d21-0410-b85c-fa13d367221e

--- trunk/Framework/Module/Main/Find.php
+++ trunk/Framework/Module/Main/Find.php
@@ -21,13 +21,25 @@ class Framework_Module_Main_Find extends ToasterAdmin_Auth_System
         if (!$form->validate()) {
             return $this->find();
         }
+        $this->setData('findForm', $this->findForm()->toHtml());    
+        $domain = $form->getElementValue('domain');
+        $this->setData('domain', $domain);
+
+        $result = $this->user->findDomain($domain);
+        if ($result == null) {
+            $this->setData('message', _('Error: does not exist'));
+        } else {
+            $this->setData('found', 1);
+            $this->setData('delete_url', "./?module=Domains&amp;event=delDomain&amp;domain=$domain");
+            $this->setData('edit_url', "./?module=Domains&amp;class=Menu&amp;domain=$domain");
+        }
         // Display find form
         $this->tplFile = 'find.tpl';
         return;
     }
     private function findForm()
     {
-        $form = new HTML_QuickForm('formFind', 'post', './?module=Main&class=Findevent=findNow');
+        $form = new HTML_QuickForm('formFind', 'post', './?module=Main&class=Find&event=findNow');
 
         $form->addElement('text', 'domain', _('Domain'));
         $form->addElement('submit', 'submit', _('Find Domain'));
--- trunk/Framework/Module/Main/Templates/Default/find.tpl
+++ trunk/Framework/Module/Main/Templates/Default/find.tpl
@@ -7,6 +7,12 @@
 <div class="box">
 {$findForm}
 <br />
+{if $found}
+<br />
+<a href="{$delete_url}">delete "{$domain}"</a> | <a href="{$edit_url}">edit "{$domain}"</a>
+<br />
+<br />
+{/if}
 <a href="./?module=Main">{$LANG_Main_Menu}</a>
 </div>
 <div class="boxbottom"></div>
