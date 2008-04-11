44f04c305e98954f098be4545328ebec969dacb7
shupp <shupp@d814430e-4d21-0410-b85c-fa13d367221e>
2008-04-11 15:55:28 +0000

    CS fixes to ToasterAdmin_Common.  Added todo
    git-svn-id: file:///Users/bill/svn/mboxsvn/toasteradmin@271 d814430e-4d21-0410-b85c-fa13d367221e

--- trunk/ToasterAdmin/Common.php
+++ trunk/ToasterAdmin/Common.php
@@ -1,5 +1,34 @@
 <?php
+/**
+ * ToasterAdmin_Common 
+ * 
+ * PHP Version 5.1.0+
+ * 
+ * @uses Framework_Auth_User
+ * @abstract
+ * @category  Mail
+ * @package   ToasterAdmin
+ * @author    Bill Shupp <hostmaster@shupp.org> 
+ * @copyright 2008 Bill Shupp
+ * @license   GPL 2.0  {@link http://www.gnu.org/licenses/gpl.txt}
+ * @link      http://trac.merchbox.com/trac/toasteradmin
+ * @todo      Check arguments to sameDomain() - first argument is not used
+ */
 
+/**
+ * ToasterAdmin_Common 
+ * 
+ * Common helper functions used by modules.
+ * 
+ * @uses Framework_Auth_User
+ * @abstract
+ * @category  Mail
+ * @package   ToasterAdmin
+ * @author    Bill Shupp <hostmaster@shupp.org> 
+ * @copyright 2008 Bill Shupp
+ * @license   GPL 2.0  {@link http://www.gnu.org/licenses/gpl.txt}
+ * @link      http://trac.merchbox.com/trac/toasteradmin
+ */
 abstract class ToasterAdmin_Common extends Framework_Auth_User
 {
 
@@ -19,23 +48,38 @@ abstract class ToasterAdmin_Common extends Framework_Auth_User
      * @access public
      * @return void
      */
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
         if (isset($_REQUEST['domain'])) {
             $this->domain = $_REQUEST['domain'];
         }
         $this->setData('domain', $this->domain);
-        $this->setData('domain_url', htmlspecialchars('./?module=Domains&class=Menu&domain=' . $this->domain));
+        $durl = './?module=Domains&class=Menu&domain=' . $this->domain;
+        $this->setData('domain_url', htmlspecialchars($durl));
     }
     
 
-    public function paginate($total) {
+    /**
+     * paginate 
+     * 
+     * Set pagination data
+     * 
+     * @param mixed $total total number of items
+     * 
+     * @access public
+     * @return void
+     */
+    public function paginate($total)
+    {
         $this->setData('total', $total);
         $this->setData('limit', (integer)Framework::$site->config->maxPerPage);
         $start = !empty($_REQUEST['start']) ? (int)$_REQUEST['start'] : 0;
         $this->setData('start', $start);
-        $this->setData('currentPage', ceil($this->data['start'] / $this->data['limit']) + 1);
-        $this->setData('totalPages', ceil($this->data['total'] / $this->data['limit']));
+        $this->setData('currentPage',
+            ceil($this->data['start'] / $this->data['limit']) + 1);
+        $this->setData('totalPages',
+            ceil($this->data['total'] / $this->data['limit']));
     }
 
     /**
@@ -44,14 +88,16 @@ abstract class ToasterAdmin_Common extends Framework_Auth_User
      * Simple wrapper for isDomainAdmin that can be called in constructors
      * 
      * @access protected
-     * @return mixed PEAR_Error if they do NOT have domain admin privs, false if they do
+     * @throws Framework_Exception on failure
+     * @return void
      */
-    protected function noDomainPrivs() {
+    protected function noDomainPrivs()
+    {
         // Verify that they have access
         if (!$this->user->isDomainAdmin($this->domain)) {
-            return PEAR::raiseError(_('Error: you do not have edit privileges on domain ') . $this->domain);
+            $message = _('Error: you do not have edit privileges on domain ');
+            throw new Framework_Exception($message . $this->domain);
         }
-        return false;
     }
 
     /**
@@ -63,7 +109,8 @@ abstract class ToasterAdmin_Common extends Framework_Auth_User
      * @return void
      * @throws Framework_Exception on failure
      */
-    protected function noDomainSupplied() {
+    protected function noDomainSupplied()
+    {
         if (is_null($this->domain)) {
             throw new Framework_Exception (_('Error no domain supplied'));
         }
@@ -72,17 +119,20 @@ abstract class ToasterAdmin_Common extends Framework_Auth_User
     /**
      * sameDomain 
      * 
-     * @param mixed $name 
-     * @param mixed $value 
+     * Compare email domin to $this->value
+     * 
+     * @param mixed $name  not sure
+     * @param mixed $value email address
+     * 
      * @static
      * @access public
      * @return void
      */
-    static public function sameDomain ($name, $value) {
+    static public function sameDomain($name, $value)
+    {
         $emailArray = explode('@', $value);
         if ($emailArray[1] == $this->domain) return true;
         return false;
     }
-
 }
 ?>
