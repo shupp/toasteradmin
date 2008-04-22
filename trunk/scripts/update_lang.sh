256e8c99f33ba6ea2cebae9366e29a774a9e3829
shupp <shupp@d814430e-4d21-0410-b85c-fa13d367221e>
2008-04-22 04:02:08 +0000

    Updated update_lang.sh script
    git-svn-id: file:///Users/bill/svn/mboxsvn/toasteradmin@294 d814430e-4d21-0410-b85c-fa13d367221e

--- trunk/scripts/update_lang.sh
+++ trunk/scripts/update_lang.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-LCDIR='../Framework/Site/Default/locale/en/LC_MESSAGES'
+LANGS='it  nl  pt  tr'
+LCBASE='../Framework/Site/Default/locale'
+LCDIR="$LCBASE/en/LC_MESSAGES"
 PHPFILE="$LCDIR/PHPmessages.po"
 SMARTYFILE="$LCDIR/SMARTYmessages.po"
 MFILE="$LCDIR/messages.po"
@@ -23,7 +25,23 @@ sed -i -e 's!FULL NAME <EMAIL@ADDRESS>!Bill Shupp <hostmaster@shupp.org>!' $MFIL
 (cd $LCDIR ; msgfmt messages.po)
 # cleanup
 rm $PHPFILE
+rm $LCBASE/en/LC_MESSAGES/messages.po-e
+rm $LCBASE/en/LC_MESSAGES/SMARTYmessages.po-e
+rm $LCBASE/en/LC_MESSAGES/PHPmessages.po-e
 rm $SMARTYFILE
 rm $TEMPFILE.php
 rm $TEMPFILE.tpl
 rm $TEMPFILE.smarty
+
+# update the non-en files
+for i in $LANGS; do
+    echo "merging $i ... "
+    LANGDIR="$LCBASE/$i/LC_MESSAGES"
+    mv $LANGDIR/messages.po $LANGDIR/messages.po.old
+    msgmerge -o $LANGDIR/messages.po \
+        $LANGDIR/messages.po.old \
+        $LCBASE/en/LC_MESSAGES/messages.po
+    echo "compiling $i ... "
+    (cd $LANGDIR ; msgfmt -v --check messages.po)
+    rm $LANGDIR/messages.po.old
+done
